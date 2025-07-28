4a77131b45f2e60bebf733ead53a0e3f59e048ee
Koncz Péter <kpeti962@gmail.com>
2025-07-29 00:10:15 +0200

    -

--- app/api/myvinyls/[id]/route.tsx
+++ app/api/myvinyls/[id]/route.tsx
@@ -3,18 +3,14 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function DELETE(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
-  const id = params.id;
+  const { id } = context.params;
 
   try {
     await prisma.wishedVinyls.delete({ where: { id: Number(id) } });
-
     return NextResponse.json({ message: 'Törlés sikeres' }, { status: 200 });
   } catch (error) {
-    return NextResponse.json(
-      { error: 'Hiba történt a törlés során' },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: 'Hiba történt a törlés során' }, { status: 500 });
   }
 }
